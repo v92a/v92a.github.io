b19ac4da260fa555e3eb4e2de47348ccd32dda9b
V92A <vitaly92a@gmail.com>
2020-12-21 01:23:14 +0300

    Some Adds

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* JavaScript - Main Script*/
 
 // init AOS.js
-AOS.init();
-console.log("Thank you {michalsnik.github.io} for AOS library :)");
+
+// AOS.init();
+// console.log("Thank you {michalsnik.github.io} for AOS library :)");
